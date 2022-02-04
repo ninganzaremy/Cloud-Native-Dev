eb80b0b75b563fa464031bdbe7b21c3ab4e22661
Remy Ninganza <rninganz@asurite.asu.edu>
2022-02-04 10:14:12 -0700

    implementing a health check to a MicroProfile app

--- src/main/java/com/example/demo/EnvHealth.java
+++ src/main/java/com/example/demo/EnvHealth.java
@@ -5,9 +5,10 @@ import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
-
-public class EnvHealth {
-
+@Health
+@ApplicationScoped
+public class EnvHealth implements HealthCheck{
+    @Override
    public HealthCheckResponse call() {
       String customEnv = System.getenv("MY_CUSTOM_ENV");
       if (customEnv != null) {
